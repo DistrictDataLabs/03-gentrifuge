1239ffeb4a98081c571a8d6d72af7c3e6c5296aa
Saran Ahluwalia <sahluwalia@Sarans-MacBook-Pro.local>
2015-08-01 10:31:53 -0400

    thin wrapper for zillow

---
+++ gentri_zillow/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name = "gentri_zillow",
+    packages = ["gentri_zillow"],
+    version = "0.0.1",
+    description = "A thin Python wrapper around the Zillow REST API",
+    author = "Gentriguge Team 3",
+    author_email = "ahlusar.ahluwalia@gmail.com",
+    url = "https://github.com/DistrictDataLabs/03-gentrifuge",
+    classifiers = [
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+)
